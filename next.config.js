67ab598f7a656050fdd1b2de4a38154866226970
breezyx28 <mohamedx.28@gmail.com>
2024-05-29 18:43:53 +0200

    added new library 'framer-motion, tailwind-merge'

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const { i18n } = require("./next-i18next.config");
 
 // module.exports = withTM({});
 const nextConfig = {
-  i18n,
+  // i18n,
   // output: "export",
   experimental: {
     appDir: true,
